d634b35e72cbd923eca6cb92db8d665963a7974e
Cody Scott <cody.j.b.scott@gmail.com>
2015-06-06 09:11:44 -0400

    Adding Swagger Docs

--- debt/urls.py
+++ debt/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^api/v1.0/', include('api.urls')),
-
+    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
