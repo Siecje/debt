93da0409bde1814fcd1a792bd600c1033ba8c364
Cody Scott <cody.j.b.scott@gmail.com>
2022-02-22 19:43:47 -0500

    Keep reference to authenticated client and set it before each test

--- api/tests/base.py
+++ api/tests/base.py
@@ -1,5 +1,5 @@
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient, APITestCase
 
 from api.models import User
 
@@ -21,12 +21,14 @@ class APIBaseTest(APITestCase):
         token = Token.objects.get(user__username=cls.user.username)
         cls.token_key = token.key
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.api_client = APIClient()
+        cls.api_client.credentials(HTTP_AUTHORIZATION='Token ' + cls.token_key)
+
     def setUp(self):
         super().setUp()
         # self.client is created before every test
-        # So can't set this in setUpClass() (after setUpTestData() sets token_key)
-        # Could create another client in setUpClass() to only set this once
-        # but then you have to use self.api_client instead of self.client
-        # Setting this before every test shouldn't be very slow right now
-        # Maybe set it once if tests are slow
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token_key)
+        # This allows the tests to use self.client
+        self.client = self.api_client
